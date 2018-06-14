4bf600bceebd59854c9cfefab9869150ab8ea842
Chenzanshang <d04220282@gm.tut.edu.tw>
2018-06-14 16:11:44 +0800

    0614

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 class ShopItem extends React.Component {
+	
+	clickDel(){
+		this.props.delItem(this.props.index);
+	}
+	
 	render() {
 		return (
 			<tr>
 				<td>{this.props.value}</td>
-				<td>000</td>
+				<td><button onClick={this.clickDel.bind(this)}>刪除</button></td>
+				
 			</tr>	
 		)
 	}
@@ -16,27 +22,46 @@ class App extends React.Component {
 		super(props);
 		this.state ={
 			items: ['ththt','isisis','aaaaaa','boooooooook'],
-			others: 123456789
+			sample: '原來'
 		};
 		this.testClick = this.testClick.bind(this);
+		this.changeText = this.changeText.bind(this);		
+	}
+	changeText ( event ) {
+		this.setState( {sample: event.target.value});
 	}
 	testClick() {
 		const data = this.state.items;
-		data.push('1111111111111111111111111');
-		this.setState( {items:data} );
+		data.push(this.state.sample );
+		this.setState( {
+			items:data,
+			sample: " "
+			
+		});
+	}
+	
+	delItem(index){
+		const data = this.state.items;
+		data.splice(index, 1);
+		this.setState( { items: data });
 	}
 	
 	render() {
 		return (
 			<div>
+			< input type="text" value={this.state.sample}
+						onChange={this.changeText} />
+				<button onClick={this.testClick}>按鈕</button>
 				<table>
 					<tbody>
-						{this.state.items.map( item =>(
-							<ShopItem value={item} />
+						{this.state.items.map(( item,idx )=>(
+							<ShopItem value={item} index={idx} 
+								delItem={this.delItem.bind(this)}	/>
 						))}
 					</tbody>       
 				</table>
-				<button onClick={this.testClick}>按鈕</button>
+				<span>{this.state.sample}</span>
+				
 			</div>		
 		)
 	}
